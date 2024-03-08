a1cf00ffa58f2c9fa8d4f7b6415ce3dfeed8e752
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-03-08 16:43:10 +0700

    demo for Part I

---
+++ demo/src/index.js
@@ -0,0 +1,41 @@
+function outScreen(x, y) {
+  return console.log(x, y);
+}
+
+// create variable and value
+const a = 5; //type number
+const b = "thanh123456"; // type string
+let c; // type freedom
+const arr = []; // array arr empty
+
+//demo lenght in string
+function outLog() {
+  for (let i = 0; i < b.length; i++) {
+    arr.push(b[i]); // push value to string b to arr
+  }
+}
+
+// print index of value arr + "arr"
+function arrLog() {
+  for (let i = 0; i < arr.length; i++) {
+    document.write(i + " " + "arr");
+  }
+}
+
+//add value for array arr
+function arrPop() {
+  document.write(arr.shift());
+}
+
+// create value for variable c type string
+c = "this is demo value and type";
+outScreen(c); // y empty so it console undefined
+console.log(a + " " + b);
+// create value for variable c type number
+c = 4;
+outScreen(c,c); // y empty so it console undefined
+console.log(b);
+console.log(b.length);
+outLog();
+console.log("this is vaule arr");
+console.log(arr);
