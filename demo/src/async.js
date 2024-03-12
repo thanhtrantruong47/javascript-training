3f601e1c596202571316e2213009f98cd7c7fdcd
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-03-12 14:56:35 +0700

    create example for async await

---
+++ demo/src/async.js
@@ -0,0 +1,29 @@
+const promise = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    resolve("success");
+  }, 4000);
+});
+
+const show = async () => {
+  let data = await promise;
+  console.log(data);
+  setTimeout(() => {
+    console.log("ok");
+  }, 4000);
+  data = await getData;
+};
+
+show();
+
+const arr = [1, 2, 3, 6, 5, 8, 9, 4, 1, 89, 45, 22, 55, 66, 3, 3, 120];
+
+const getData = new Promise(function (resolve, reject) {
+  let i = 0;
+  setTimeout(() => {
+    console.log("item");
+    while (i < arr.length) {
+      console.log(arr[i]);
+      i++;
+    }
+  }, 10000);
+});
