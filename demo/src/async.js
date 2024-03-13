a13839672a3ca9dbe9f319af69b929d9d8a8ffa9
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-03-13 17:25:41 +0700

    create more example for async

--- demo/src/async.js
+++ demo/src/async.js
@@ -9,10 +9,8 @@ const show = async () => {
   // will await 4s to log "ok"
   // and after 2s will log getValue
   let data = await promise;
+  console.log("ok");
   console.log(data);
-  setTimeout(() => {
-    console.log("ok");
-  }, 4000);
   let data1 = await getData;
 };
 
@@ -22,11 +20,40 @@ const arr = [1, 2, 3, 6, 5, 8, 9, 4, 1, 89, 45, 22, 55, 66, 3, 3, 120];
 
 const getData = new Promise(function (resolve, reject) {
   let i = 0;
-  setTimeout(() => {
     console.log("item");
     while (i < arr.length) {
       console.log(arr[i]);
       i++;
     }
-  }, 10000);
 });
+
+function value() {
+  return new Promise((reject) => {
+    setTimeout(() => {
+      reject("this promise erro");
+    }, 2000);
+  });
+}
+
+function value1() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("this promise1");
+    }, 2000);
+  });
+}
+
+const getValue = async () => {
+  try {
+    console.log("this is a");
+    let data = await value();
+    console.log(data);
+    console.log("this is c");
+    data = await value1();
+    console.log(data);
+  } catch (erro) {
+    console.log(data);
+  }
+};
+
+getValue();
