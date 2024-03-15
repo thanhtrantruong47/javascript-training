ce5b5bc56c9d2522d4317a056e3c474dc15fc528
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-03-15 10:44:21 +0700

    create navbar and button number example add num for cart

--- Modify/src/index.js
+++ Modify/src/index.js
@@ -1,30 +1,51 @@
-
 // Modifying Attributes
 
-const demoImg = document.querySelector('#demo');
+const demoImg = document.querySelector("#demo");
 
-console.log(demoImg.hasAttribute('src'))
+console.log(demoImg.hasAttribute("src"));
 
-console.log(demoImg.getAttribute('src'))
-demoImg.removeAttribute('src')
-console.log(demoImg.hasAttribute('src'))
-demoImg.setAttribute('src','https://dulichsinhcafe.com.vn/wp-content/uploads/2017/11/anhr-cafe-sapa.jpg')
-console.log(demoImg.hasAttribute('src'))
+console.log(demoImg.getAttribute("src"));
+demoImg.removeAttribute("src");
+console.log(demoImg.hasAttribute("src"));
+demoImg.setAttribute(
+  "src",
+  "https://dulichsinhcafe.com.vn/wp-content/uploads/2017/11/anhr-cafe-sapa.jpg"
+);
+console.log(demoImg.hasAttribute("src"));
 
 // Modifying Attributes class
 
-const firstDiv = document.querySelector('div')
-firstDiv.style.backgroundColor = 'blue'
-firstDiv.classList.add('div', 'light', 'hidden')
-console.log(firstDiv)
+const demoBtn = document.querySelector(".show");
+demoBtn.setAttribute('style', 'boder-radius: 20px')
 
-const demoClass = document.querySelector('.demo');
-console.log(demoClass)
-demoClass.classList.toggle('hidden')
 
-const demoBtn = document.querySelector('.btn');
+const demoNav = document.querySelector(".nav");
 
-demoBtn.addEventListener('click', () => {
-  demoClass.classList.toggle('hidden')
-  demoClass.classList.replace('demo', 'light')
-})
+demoNav.classList.add("hidden"); // add hidden for nav the nav will hidden in page
+demoNav.classList.toggle("hidden"); // hidden opened behave this code so when call this line code the hidden will off and navbara show in page
+
+demoBtn.addEventListener("click", () => {
+  demoNav.classList.toggle('hidden') // when click btn class hidden will onpen and off this navbar will show and hidden for page :))
+});
+
+const actionMinus = document.querySelector(".minus");
+const actionPlus = document.querySelector(".plus");
+let showNum = document.querySelector(".num");
+
+console.log(actionMinus.classList.contains("king")); // console log = false
+
+let a = 0; // create varibale num
+
+// the func check num > 0 when click num - 1 if num = 0 when click num alway = 0
+actionMinus.addEventListener("click", () => {
+  if (a > 0) {
+    a = a - 1;
+  }
+  document.querySelector(".num").innerHTML = a;
+});
+
+actionPlus.addEventListener("click", () => {
+  a = a + 1;
+  showNum.innerHTML = a;
+  document.querySelector(".num").innerHTML = a;
+});
