0c61999976a9c8e6081da1448ea8d2cb5123bfbe
thanhtrantruong47 <thanh.trantruong47@gmail.com>
2024-03-18 10:38:42 +0700

    slit file js form file index.html

---
+++ Exercise 3/index.js
@@ -0,0 +1,49 @@
+const txt = document.querySelector(".js-task-name");
+const numberTask = document.querySelector(".js-pomodoro-count");
+const insert_Row = () => {
+  let myTbl = document.querySelector(".js-task-table-body ").insertRow(0); //create new row tabl
+  let row1 = myTbl.insertCell(0);
+  let row2 = myTbl.insertCell(1);
+  let doing = 0;
+  let createNum = 0;
+  let btnDone = document.createElement("BUTTON");
+  btnDone.innerHTML = "Done";
+  let btnCreate = document.createElement("BUTTON");
+  btnCreate.innerHTML = "Create";
+  let btnDelete = document.createElement("BUTTON");
+  btnDelete.innerHTML = "Delete";
+  let textFinsh = document.createElement("div");
+  textFinsh.innerHTML = "finish";
+  textFinsh.style.display = "none";
+  const up = " / ";
+  row1.innerHTML = txt.value;
+  row2.innerHTML = doing + up + numberTask.value + " pomodori";
+  let row3 = myTbl.insertCell(2);
+  row3.style.display = "flex";
+  row3.style.gap = "10px";
+  row3.appendChild(textFinsh);
+  row3.appendChild(btnDone);
+  row3.appendChild(btnCreate);
+  row3.appendChild(btnDelete);
+
+  btnDone.addEventListener("click", () => {
+    const finish = "finsh";
+    if (doing < numberTask.value) {
+      doing = doing + 1;
+      row2.innerHTML = doing + up + numberTask.value + " pomodori";
+    } else {
+      textFinsh.style.display = "block";
+      btnDone.style.display = "none";
+      btnCreate.style.display = "none";
+    }
+  });
+  btnCreate.addEventListener("click", () => {
+
+    doing = 0;
+    row2.innerHTML = doing + up + createNum + " pomodori";
+  });
+
+  btnDelete.addEventListener("click", () => {
+  myTbl.remove(myTbl.row1);
+});
+};
